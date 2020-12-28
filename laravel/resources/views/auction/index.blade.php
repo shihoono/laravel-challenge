01877b6e63287b14a1a741642cb64d8e112ff888
shihoono <shiho1449@gmail.com>
2020-12-28 17:32:44 +0900

    auction/index ページ

---
+++ laravel/resources/views/auction/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h2>ミニオークション!</h2>
+    <h3>*出品されている商品</h3>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Finished</th>
+                    <th>Endtime</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($biditems as $biditem)
+                <tr>
+                    <td>{{ $biditem->name }}</td>
+                    <td></td>
+                    <td>{{ $biditem->endtime }}</td>
+                    <td></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+@endsection
