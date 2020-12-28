f70f31ec54474e99c98fdb892ea573a873d35b2a
shihoono <shiho1449@gmail.com>
2020-12-28 19:24:07 +0900

    ルーティング変更

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+    Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 });
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::get('/', 'AuctionController@index');
+Route::resource('auction', 'AuctionController');
