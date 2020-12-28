e6950d06f5027fde28b4c6450de20b21482daf79
shihoono <shiho1449@gmail.com>
2020-12-28 19:33:41 +0900

    auction/indexはログインした人のみ閲覧可能

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -20,5 +20,7 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::get('/', 'AuctionController@index');
-Route::resource('auction', 'AuctionController');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', 'AuctionController@index');
+    Route::resource('auction', 'AuctionController');
+});
