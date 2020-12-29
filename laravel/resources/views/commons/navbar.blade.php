7d70fa001713dd66e3a01b087b0242d4b22b2a1b
shihoono <shiho1449@gmail.com>
2020-12-29 20:28:41 +0900

    ログインユーザーの名前表示

--- laravel/resources/views/commons/navbar.blade.php
+++ laravel/resources/views/commons/navbar.blade.php
@@ -1,11 +1,7 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Auction</a>
-
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <a class="navbar-brand" href="/">Auction [ {{ Auth::user()->name }} ]</a>
 
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
