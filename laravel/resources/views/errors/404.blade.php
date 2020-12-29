4f640bbd834f21551c6d42b73bc2c9d30a9e2da0
shihoono <shiho1449@gmail.com>
2020-12-29 20:54:38 +0900

    404エラーページ　作成

---
+++ laravel/resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('auth.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <div class="text-center">
+          <p>お探しのページは見つかりませんでした。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
