9ee78eebb7ef57e800feb7f9b5f41d5697e23ee9
shihoono <shiho1449@gmail.com>
2020-12-31 00:58:25 +0900

    日付フォーマット変更処理　修正

--- laravel/app/Biditem.php
+++ laravel/app/Biditem.php
@@ -3,15 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Biditem extends Model
 {
     protected $fillable = ['name', 'description', 'endtime'];
 
-    protected $dates = ['endtime'];
-
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getFormattedEndTimeAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['endtime'])
+            ->format('Y/n/d g:i A');
+    }
+
+    public function getFormattedCreatedAtAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['created_at'])
+            ->format('Y/n/d g:i A');
     }
 }
