7e9f8158362aab5175ee800a69c61ea7994b1ccc
shihoono <shiho1449@gmail.com>
2021-01-02 01:05:20 +0900

    ルーティング　追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/', 'AuctionController@index');
     Route::resource('auction', 'AuctionController');
     Route::get('home2', 'AuctionController@home2')->name('auction.home2');
+    Route::get('home', 'AuctionController@home')->name('auction.home');
     Route::get('{id}/bidform', 'AuctionController@bidForm')->name('auction.bidform');
     Route::post('bid', 'AuctionController@bid')->name('auction.bid');
 });
