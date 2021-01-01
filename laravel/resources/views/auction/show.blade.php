8b697f065d32be7f8b3368ee04f72335ef221ce0
shihoono <shiho1449@gmail.com>
2021-01-02 00:23:56 +0900

    入札情報はあれば表示するよう変更

--- laravel/resources/views/auction/show.blade.php
+++ laravel/resources/views/auction/show.blade.php
@@ -40,51 +40,53 @@
 
     <h4>落札情報</h4>
     @if($bidinfo->isNotEmpty())
-        @foreach ($bidinfo as $bidinfo)
-            <table class="table table-striped" style="margin-bottom:40px;">
-                <thead>
-                    <tr>
-                        <th>落札者</th>
-                        <th>落札金額</th>
-                        <th>落札日時</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <table class="table table-striped" style="margin-bottom:40px;">
+            <thead>
+                <tr>
+                    <th>落札者</th>
+                    <th>落札金額</th>
+                    <th>落札日時</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($bidinfo as $bidinfo)
                     <tr>
                         <td>{{ $bidinfo->user->name }}</td>
                         <td>{{ $bidinfo->price }}円</td>
                         <td>{{ $bidinfo->formatted_created_at }}</td>
                     </tr>
-                </tbody>
-            </table>
-        @endforeach
+                @endforeach
+            </tbody>
+        </table>
     @elseif($bidinfo->isEmpty())
-    <p>落札はありません。</p>
+        <p>落札はありません。</p>
     @endif
 
     <h4>入札情報</h4>
-
     @if($biditem->finished === 0)
         {!! link_to_route('auction.bidform', '<入札する>', ['id' => $biditem->id]) !!}
     @endif
 
-    <table class="table table-striped" style="margin-top:10px;">
-        <thead>
-            <tr>
-                <th>入札者</th>
-                <th>金額</th>
-                <th>入札日時</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($bidrequests as $bidrequest)
+    @if($bidrequests->isNotEmpty())
+        <table class="table table-striped" style="margin-top:10px;">
+            <thead>
                 <tr>
-                    <td>{{ $bidrequest->user->name }}</td>
-                    <td>{{ $bidrequest->price }}円</td>
-                    <td>{{ $bidrequest->formatted_created_at}}</td>
+                    <th>入札者</th>
+                    <th>金額</th>
+                    <th>入札日時</th>
                 </tr>
-                @endforeach
-            </tbody>
-    </table>
-
+            </thead>
+            <tbody>
+                @foreach($bidrequests as $bidrequest)
+                    <tr>
+                        <td>{{ $bidrequest->user->name }}</td>
+                        <td>{{ $bidrequest->price }}円</td>
+                        <td>{{ $bidrequest->formatted_created_at}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+        </table>
+    @elseif($bidrequests->isEmpty())
+        <p>入札はありません。</p>
+    @endif
 @endsection
