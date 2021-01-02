c3daee3eed6b3b78f91a2ce148022baed4618e7f
shihoono <shiho1449@gmail.com>
2021-01-02 10:19:09 +0900

    Bidmessageモデル　作成

---
+++ laravel/app/Bidmessage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class Bidmessage extends Model
+{
+    protected $fillable = ['message', 'user_id', 'biditem_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bidinfo()
+    {
+        return $this->belongsTo(Biditem::class);
+    }
+
+    public function getFormattedCreatedAtAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['created_at'])
+            ->format('Y/n/d g:i A');
+    }
+}
