153d666969341b3416769dd19d9db99f964dafa5
shihoono <shiho1449@gmail.com>
2021-01-04 18:41:22 +0900

    ファイル名変更

--- laravel/app/Http/Requests/CreateBiditemRequest.php
+++ laravel/app/Http/Requests/CreateBiditemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateBiditem extends FormRequest
+class CreateBiditemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -30,5 +30,5 @@ class CreateBiditem extends FormRequest
             'endtime' => 'required|after:now',
         ];
     }
-    
+
 }
