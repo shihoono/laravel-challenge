f69adec8770c1751942d78ef510c768bc78f75e0
shihoono <shiho1449@gmail.com>
2021-01-05 21:11:39 +0900

    評価済みかどうかチェック

--- laravel/app/Http/Controllers/ReviewsController.php
+++ laravel/app/Http/Controllers/ReviewsController.php
@@ -29,21 +29,31 @@ class ReviewsController extends Controller
         $bidinfo = Bidinfo::findOrFail($request->id);
         $biditem = Biditem::findOrFail($bidinfo->biditem_id);
 
-        if($bidinfo->trading_status === 2){
-            $review->bidinfo_id = $request->id;
-            $review->reviewer_id = $user->id;
-            
-            if($bidinfo->user_id === $user->id){
-                $review->reviewee_id = $biditem->user_id;
-            }
-            if($biditem->user_id === $user->id){
-                $review->reviewee_id = $bidinfo->user_id;
-            }
+        //評価済みかどうか
+        $reviewed = Review::where([
+            ['bidinfo_id', $bidinfo->id],
+            ['reviewer_id', $user->id]
+        ])->count();
 
-            $review->rate = $request->rate;
-            $review->comment = $request->comment;
-            if($review->save()){
-                return redirect('/')->with('flash_success', '評価しました');
+        if($bidinfo->trading_status === 2){
+            if($reviewed === 0){
+                $review->bidinfo_id = $request->id;
+                $review->reviewer_id = $user->id;
+                
+                if($bidinfo->user_id === $user->id){
+                    $review->reviewee_id = $biditem->user_id;
+                }
+                if($biditem->user_id === $user->id){
+                    $review->reviewee_id = $bidinfo->user_id;
+                }
+
+                $review->rate = $request->rate;
+                $review->comment = $request->comment;
+                if($review->save()){
+                    return redirect('/')->with('flash_success', '評価しました');
+                }
+            } else {
+                return back()->with('flash_error', '評価済みです');
             }
         } else {
             return back()->with('flash_error', 'まだ取引が完了していません');
