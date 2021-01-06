2b44fa9487980791cea0534721b8c46fb482526f
shihoono <shiho1449@gmail.com>
2021-01-06 13:03:45 +0900

    不要ファイル削除

--- laravel/app/Http/Controllers/AuctionController.php
+++ laravel/app/Http/Controllers/AuctionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateBiditemRequest;
-use App\Http\Requests\AfterBidRequest;
 
 use App\Biditem;
 use App\User;
